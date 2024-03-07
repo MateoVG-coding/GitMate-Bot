2c8b39d280728d61428a58849c40f139fb60eab8
MateoVG-coding <mateovega10@hotmail.com>
2024-03-07 09:57:45 -0500

    Remove icon parameter from webhook commands.

--- src/cogs/webhook_manager.py
+++ src/cogs/webhook_manager.py
@@ -1,4 +1,3 @@
-from io import BytesIO
 import requests
 import discord
 from discord.ext import commands
@@ -17,31 +16,20 @@ class WebhookManager(commands.Cog):
     @webhook_mang.command(name='create', description='Create a webhook for a Discord channel')
     @option("channel", description="Enter the channel for this webhook")
     @option("name", description="Enter the name for the webhook")
-    @option("icon-url", description="Enter the icon URL for this webhook", required=False)
-    async def create_webhook(self, ctx, channel: discord.TextChannel, name, icon_url):
+    async def create_webhook(self, ctx, channel: discord.TextChannel, name):
         """Command to create a webhook"""
 
         if not channel.permissions_for(ctx.author).manage_webhooks:
             return await ctx.send("😕 *Sorry, it seems you do not have permission to manage webhooks in this channel.*")
 
-        if icon_url:
-            if not is_valid_icon_url(icon_url):
-                return await ctx.respond("❌ *Sorry, it seems the icon url you provided is invalid.*", ephemeral=True)
-
-            response = requests.get(icon_url, timeout=30)
-            icon_bytes = BytesIO(response.content)
-            avatar = icon_bytes.read()
-        else:
-            icon_bytes = None
-
-        webhook = await channel.create_webhook(name=name, avatar=avatar)
+        webhook = await channel.create_webhook(name=name)
         body = f"""◉ **Url:** ```{webhook.url}```
                    ◉ **Id:** `{webhook.id}`
                    ◉ **Channel:** {channel.mention}"""
 
         embed = discord.Embed(colour=0x541dd3, description=body)
         embed.set_author(name=f"Webhook {webhook.name}",
-                        url=f"{webhook.url}", icon_url=icon_url)
+                        url=f"{webhook.url}")
 
         await ctx.respond(embed=embed, ephemeral=True)
 
